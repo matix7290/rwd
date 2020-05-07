aa08a8669f8511f1bc999fa6fc2002f715bda5e3
matix7290 <mateusz7290.mo@gmail.com>
2020-05-07 02:25:49 +0200

    first upload

---
+++ search.js
@@ -0,0 +1,14 @@
+const searchInput = document.querySelector("#szukaj");
+const res = [...document.querySelectorAll(".all")];
+const portfolio = document.querySelector(".portfolio");
+
+const searchWord = e => {
+	const currentWord = e.target.value.toLowerCase();
+	let resoult = res;
+	resoult = resoult.filter(res => res.textContent.toLowerCase().includes(currentWord));
+	portfolio.textContent = '';
+	resoult.forEach(name => portfolio.appendChild(name));
+	console.log(resoult)
+}
+
+searchInput.addEventListener('input', searchWord);
